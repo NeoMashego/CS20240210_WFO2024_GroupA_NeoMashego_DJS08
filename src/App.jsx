8a02a67af4a75b1ef5adbfeaec8e97f053428ca7
NeoMashego <mashegonoj@gmail.com>
2024-11-14 16:12:45 +0200

    links added and edited spelling errors

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,25 @@
  * linked in the slides.
  */
 
-import { BrowserRoouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 
 function App() {
 
   return (
-    <BrowserRoouter>
+    <BrowserRouter>
+      <header>
+        <Link to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
       <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
       </Routes>
-    </BrowserRoouter>
+    </BrowserRouter>
   )
 }
 
