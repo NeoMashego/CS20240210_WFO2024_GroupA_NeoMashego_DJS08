9b66d1f3c77d03d2d3f573b16a8761a7318864ee
NeoMashego <mashegonoj@gmail.com>
2024-11-15 14:03:24 +0200

    edited vans object array and added searchParams functionality

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 import Vanny from "../images/van.jpg"
 
 const van = [
@@ -12,23 +12,56 @@ const van = [
     },
     {
         id: 2,
-        color: "red",
-        type: "Station wagon",
-        registration: new Date('2018-03-03'),
+        color: "purple",
+        type: "Sedan",
+        registration: new Date('2020-08-16'),
         capacity: 5
     },
     {
         id: 3,
+        color: "red",
+        type: "Wagon",
+        registration: new Date('2018-03-03'),
+        capacity: 5
+    },
+    {
+        id: 4,
         color: "blue",
         type: "Sedan",
         registration: new Date('2019-05-03'),
         capacity: 5
+    },
+    {
+        id: 5,
+        color: "orange",
+        type: "Sedan",
+        registration: new Date('2020-08-01'),
+        capacity: 8
+    },
+    {
+        id: 6,
+        color: "green",
+        type: "Wagon",
+        registration: new Date('2021-04-04'),
+        capacity: 7
+    },
+    {
+        id: 7,
+        color: "blue",
+        type: "Wagon",
+        registration: new Date('2021-02-22'),
+        capacity: 7
     }
 ]
 
 function Vans(){
+    const [searchParams, setSearchParams] = useSearchParams()
 
-    const vanElements = van.map(v => (
+    const typeFilter = searchParams.get("type")
+
+    const displayedVans = typeFilter ? van.filter(v => v.type === typeFilter) : van
+
+    const vanElements = displayedVans.map(v => (
         <div className="van-tile" key={v.id}>
             <img src={Vanny} />
             <Link to={`/vans/${v.id}`}>
@@ -44,6 +77,12 @@ function Vans(){
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            <div className="van-list-filter-buttons">
+                <Link to="?type=Minivan" className="van-type minivan"> Minivan </Link>
+                <Link to="?type=Sedan" className="van-type sedan"> Sedan </Link>
+                <Link to="?type=Wagon" className="van-type wagon"> Wagon </Link>
+                <Link to="." className="van-type clear-filter">Clear</Link>
+            </div>
             <div className="van-list">
                 {vanElements}
             </div>
